d0a653d4bde048d4bf893ab35c77ea6064e69e2c
gemillan321 <144914936+gemillan321@users.noreply.github.com>
2026-02-18 08:56:08 +0000

    lol

--- my-profile/frontend/vite.config.js
+++ my-profile/frontend/vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/guestbook': {
-        target: 'http://localhost:5000', // forward requests to backend
-        changeOrigin: true,
-      },
-    },
-  },
 });
